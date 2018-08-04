ec1d7aa8eeb214634ac774a9c0db9a5606aa19db
Afshan Aman <afshan.aman@gmail.com>
2018-08-04 16:24:05 +0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,21 +15,10 @@
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
+        <div class="flex-center position-ref full-height">           
             <div class="content">
                 <div class="title m-b-md">
-                    <img src="images/logo.png" class="logo">The Footlady
+                    <img src="images/logo.png" class="logo">The Foot lady
                 </div>
 
                 <div class="links">
